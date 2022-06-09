2b02a12903c16a9a666eaa14234a7140e2156a85
thegreatbullet <clementlum@rocketmail.com>
2022-06-10 06:58:18 +0800

    changed default token permissions for rest

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -47,8 +47,12 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         # "rest_framework.permissions.AllowAny", allow any one to view api data
-        "rest_framework.permissions.IsAuthenticated", #only allow authenticated users to view api data
-    ]
+        "rest_framework.permissions.IsAuthenticated",  # only allow authenticated users to view api data
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
 }
 
 MIDDLEWARE = [
